a0460550467f64b9afe6ed6a44c2f7deb765816f
genBTC <genBTC@gmx.com>
2023-02-06 11:15:19 -0500

    kernel.org patch-new.sh patch changelog download upgrade script

---
+++ kernel/patch-new.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#genr8eofl @gentoo - copyleft 2021,2022,2023
+# Check Kernel.org for New Patch and Changelog,
+#  then Download and Patch linux-sources @ /usr/src/linux
+# Usage:
+#  in /usr/src/linux # ./patch-new.sh
+# Deps: bash, grep, awk, wget, xz, patch, less, sed
+
+function changelog() { grep -A 4 "^commit" "$1" | grep -E "(^    )"; }
+
+#use of eval justified to import makefile vars
+eval "$(awk -F' = ' '{print $1 "=" $2}' <(tail -n+2 Makefile | head -n3) )"
+#current base version will be auto-determined
+v=$VERSION p=$PATCHLEVEL s=$SUBLEVEL
+echo "Kernel Version, currently = $v.$p.$s (local)"
+##s1=${1-"`grep "SUBLEVEL" Makefile | head -1 | awk '{print $3}'`"} #(old way)
+s1=$s            #sublevel1 is current local version
+s2=$(( s1 + 1 )) #sublevel2 is next remote patch
+
+URL="https://cdn.kernel.org/pub/linux/kernel/v${v}.x/"
+
+function download() {
+ echo "Downloading Patch..."
+ ptf="patch-$v.$p.$s1-$s2"
+ mkdir -p patches; pushd patches || die
+ wget "${URL}/incr/${ptf}.xz"
+ if [ -f "${ptf}.xz" ]; then
+  echo "Patch ${ptf}.xz found. Decompressing patch .XZ..."
+  xz -d -T0 "${ptf}.xz"
+  echo "Now Patching..."
+  popd || die
+  mkdir -p patched
+  patch -p1 < patches/"$ptf" > patched/patched-"$s1-$s2".txt
+  echo "Patch Complete! Log Saved @ ${PWD}/patched-$s1-$s2.txt"
+ else
+  echo "Patch 404 not found, this means we're up to date. Bailing out!"
+  exit
+ fi
+ mkdir -p changelogs; pushd changelogs || die
+ echo "Downloading ChangeLog..."
+ chlog="ChangeLog-$v.$p.$s2"
+ wget "${URL}/${chlog}"
+ if [ -f "$chlog" ]; then
+  echo "ChangeLog Summary Created!"
+  changelog "$chlog" > "$chlog-summary.txt"
+  echo "ChangeLog review opening, q to quit..."
+  less "$chlog-summary.txt"
+ fi
+ popd || die
+}
+#Patch wont apply cleanly and its struggling with my  custom ver string
+sed -i 's/^EXTRAVERSION = -gentoo-hardened1/EXTRAVERSION = /' Makefile
+download #runit
+sed -i 's/^EXTRAVERSION = /EXTRAVERSION = -gentoo-hardened1/' Makefile
