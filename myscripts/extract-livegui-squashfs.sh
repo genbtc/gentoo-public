113e04750a15cdae886c2978836b486cee5234fe
genBTC <genBTC@gmx.com>
2023-07-08 21:24:24 -0400

    v0.2 extract squashfs to dir and run chroot script

--- myscripts/extract-livegui-squashfs.sh
+++ myscripts/extract-livegui-squashfs.sh
@@ -1,24 +1,32 @@
 #!/bin/sh
 # script v0.1 - created by genr8eofl @ gentoo - July 8, 2023 - AGPL3
+ISODIR="/mnt/borg/ISOs"                     #choose where the source ISO is
+ISO="livegui-amd64-20230705T183202Z.iso"    #whatever ISO we want
+imgsq="image.squashfs"  #CONSTANT           #(the image inside the ISO)
+target=${ISO%.iso}                          #target directory to extract to
 
-ISO="livegui-amd64-20230705T183202Z.iso"
-target=${ISO%.iso}
+#start extracting squashfs file from ISO (7z required to extract):
 # 7z x /mnt/borg/ISOs/livegui-amd64-20230705T183202Z.iso image.squashfs
 # mv image.squashfs livegui-amd64-20230705T183202Z.iso.squashfs
-# mv image.squashfs ${sqfs}
-sqfs="livegui-amd64-20230705T183202Z.iso.squashfs"
-gentoo="/mnt/gentoo"
+7z ${ISODIR}/${ISO} $imgsq
+mv image.squashfs  ${sqfs}
+
+sqfs="${ISO}.squashfs"
+unsquashfs -d ${sqfs%.iso.squashfs}  $sqfs
+
+#no #gentoo="/mnt/gentoo"
 #no #mount -t squashfs -o rw ${sqfs} ${gentoo}
 #no #grep ${sqfs} #  < #   /mnt/crucialp1/livegui-amd64-20230705T183202Z.iso.squashfs on /mnt/gentoo type squashfs (ro,relatime,seclabel)
-unsquashfs -d ${sqfs%.iso.squashfs}  $sqfs
+
 #cp /etc/resolv.conf /mnt/gentoo/etc/
 #cp /etc/resolv.conf  livegui-amd64-20230705T183202Z/etc
-
-#bind mount /var/db/repos
-#gentoo-chroot
-
-#emerge-webrsync
-
+cp /etc/resolv.conf $target
+#handle make.conf
 #mv /mnt/crucialp1/livegui-amd64-20230705T183202Z/etc/portage/make.conf /mnt/crucialp1/livegui-amd64-20230705T183202Z/etc/portage/make.conf.dist
 #cp gentoo-livegui-amd64-20230604T170201Z-etc-portage-make.conf /mnt/crucialp1/livegui-amd64-20230705T183202Z/etc/portage/make.conf
 
+#mount --bind /var/db/repos/ var/db/repos/
+#gentoo-chroot $target
+
+#emerge-webrsync
+#emerge -avuDU @world
