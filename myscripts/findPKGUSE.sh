1e5cb51f668effe37b70f325e0d781030c6dff04
genBTC <genBTC@gmx.com>
2023-07-09 00:40:33 -0400

    findPKGUSE.sh script v0.3 generate all my package.use entries

--- myscripts/findPKGUSE.sh
+++ myscripts/findPKGUSE.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-#script v0.2 by @genr8eofl copyright 2023 - AGPL3 License
+#script v0.3 by @genr8eofl copyright 2023 - AGPL3 License
+echo "# This file was automatically generated on $(date +"%FT%H-%M-%S")"
+echo "# a package.use file,  created by genr8eofl's findPKGUSE.sh script v0.3"
 
-#start with correct package db dir
-oldvdb=$(portageq vdb_path / )
-: ${oldvdb:=/var/db/pkg}
-#enter that directory?
-#pushd $oldvdb
-#find all metadata files named PKGUSE
-#pkguses=$(find . -name PKGUSE)
-pkguses=($(find ${oldvdb} -name PKGUSE))
-#strip out the ./ current directory from it.
-#pkguses=${pkguses#.\/}
-#pkguses=${pkguses#/var/db/pkg/}
-#pkguses=${pkguses%PKGUSE}
-#cat <<< $pkguses
-
-#create a FD with <(, loop thru PKGUSE array, print out metadata, sort
+#start with correct portage vdb dir
+vdb=$(portageq vdb_path / )
+: ${vdb:=/var/db/pkg}
+#create a FD with <(, find all PKGUSE's, loop through, print out metadata, sort
 sort -h < <(
-for apkg in "${pkguses[@]}"; do
-      cd $(dirname ${apkg})
-      echo $(cat CATEGORY)"/"$(cat PF) $(cat PKGUSE)
-done
+    #find all metadata files named PKGUSE, loop.
+    for apkg in $(find ${vdb} -name PKGUSE); do
+        cd $(dirname ${apkg})    #enter that directory
+        echo $(cat CATEGORY)"/"$(cat PF) $(cat PKGUSE)
+    done
 )
