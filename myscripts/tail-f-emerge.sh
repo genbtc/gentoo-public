71c11c9f7db42ea8e967d88885a7f422eb97b29f
genBTC <genBTC@gmx.com>
2023-07-20 03:10:17 -0400

    wtmp format boot times script

--- myscripts/tail-f-emerge.sh
+++ myscripts/tail-f-emerge.sh
@@ -1,1 +1,4 @@
+#!/bin/sh
+#script v1.0 by @genr8eofl copyright 2023 - AGPL3 License
+
 cat /var/log/emerge.log | awk  '{ $1 = strftime("%m-%d-%Y %H:%M:%S",$1); print }' "$1" | less
---
+++ myscripts/wtmp-format.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# wtmp-format-boot.sh script v1.0 by @genr8eofl copyright 2023 - AGPL3 License
+# description: Interpret the "last log" to check for boot activity, print your last boot or reboot dates in sequence
+# example output:
+#2023-07-18_08:23:09 = 6.1.13-gentoo-dist+
+#2023-07-11_20:09:56 = 5.10.186-gentoo-hardened1-ZEN3iGPU-DBG8
+
+last -w --time-format=iso | grep "system boot" | sed 's/reboot   system boot  //g' | awk '{ cmd="date --date=" $2 " +\"%Y-%m-%d_%H:%M:%S\""; cmd |& getline q; close(cmd); print q " = " $1}'
