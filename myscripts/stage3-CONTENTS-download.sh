2c9c30dec4d16c5b1e3a29953b637ddf25067114
genBTC <genBTC@gmx.com>
2023-09-29 13:44:52 -0400

    stage3-CONTENTS-download.sh

--- myscripts/stage3-CONTENTS-download.sh
+++ myscripts/stage3-CONTENTS-download.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
 # stage3-CONTENTS-download.sh script v0.1 - genr8eofl @gentoo - Sept 28, 2023
 
-WEBDIR="https://mirrors.rit.edu/gentoo/releases/amd64/autobuilds/current-stage3-amd64-hardened-openrc/"
-TODAY="Sept2823"
-DIRLIST="elinks-dump-gentoo-release-autobuilds-dirlisting-${TODAY}"
+#INPUT:
+STAGE3_AMD64="stage3-amd64"
+WEBDIR="https://mirrors.rit.edu/gentoo/releases/amd64/autobuilds/current-${STAGE3_AMD64}-hardened-openrc/"
 
 #depend on elinks to convert HTML dir listing to formatted text
+TODAY="Sept2823"
+DIRLIST="elinks-dump-gentoo-release-autobuilds-dirlisting-${TODAY}"
 elinks --dump ${WEBDIR} > ${DIRLIST}.txt
 
 #grab lines containing CONTENTS
-CONTENTS="${DIRLIST}-grep-CONTENTS"
-grep 'CONTENTS.gz' ${DIRLIST} > ${CONTENTS}.txt
+GREPCONTENTS="${DIRLIST}-CONTENTS-grep"
+grep 'CONTENTS.gz' ${DIRLIST}.txt > ${GREPCONTENTS}.tmp
 
-#throw away everything else
-AWKCONTENTS='${CONTENTS}-awk'
-awk 'NF>=3 {print $3}' ${CONTENTS}.txt > ${AWKCONTENTS}.txt
+#throw away bottom half thats not 3 columns wide
+AWKCONTENTS="${GREPCONTENTS}-CONTENTS-awk"
+awk 'NF>=3 {print $3}' ${GREPCONTENTS}.tmp > ${AWKCONTENTS}.tmp
+#OUTPUT:
 #[31]livegui-amd64-20230924T163139Z.iso.CONTENTS.gz
 #[41]stage3-amd64-desktop-openrc-20230924T163139Z.tar.xz.CONTENTS.gz
 #[51]stage3-amd64-desktop-systemd-20230924T163139Z.tar.xz.CONTENTS.gz
 
 #chop off leading [digits]
-SEDCONTENTS='${AWKCONTENTS}-sed'
-sed 's#^\[[[:digit:]]*\]##' ${AWKCONTENTS}.txt > ${SEDCONTENTS}.txt
+sed -i 's#^\[[[:digit:]]*\]##' ${AWKCONTENTS}.tmp
+#OUTPUT:
+#admincd-amd64-20230924T163139Z.iso.CONTENTS.gz
+#install-amd64-minimal-20230924T163139Z.iso.CONTENTS.gz
+#livegui-amd64-20230924T163139Z.iso.CONTENTS.gz
+#stage3-amd64-desktop-openrc-20230924T163139Z.tar.xz.CONTENTS.gz
+#stage3-amd64...
+
+SEDCONTENTS="${DIRLIST}-CONTENTS-packages"
+#filter admin/install/livegui/x32arch out
+grep "^${STAGE3_AMD64}" ${AWKCONTENTS}.tmp |
+# tee to stdout and to final result file
+  tee ${SEDCONTENTS}.txt
+
+#remove tmp files
+rm ${GREPCONTENTS}.tmp ${AWKCONTENTS}.tmp
+
+#90% of this script can be replaced by this snippet
+grep "${WEBDIR}${STAGE3_AMD64}.*CONTENTS.gz" ${DIRLIST}.txt | awk '{print $2}'
+
+#Need to download them later
