aa33315d13d988b3a3384dc7f619f8eda37da828
genBTC <genBTC@gmx.com>
2023-09-30 19:57:54 -0400

    shellcheck and improvements

--- myscripts/find-vardbpkg-earlier-BUILD_TIME.sh
+++ myscripts/find-vardbpkg-earlier-BUILD_TIME.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-#@genr8eofl @gentoo 2023-01-04 - timestampscript, find-vardbpkg-BUILD_TIME.sh
+# timestamps script, find-vardbpkg-BUILD_TIME.sh v0.3 -  by genr8eofl @ gentoo - copyright 2023 - AGPL3 License
 # Description: Gather BUILD_TIME installation emerge Timestamp from vdb varPkgDB
-# Usage: # ./find-vardbpkg-earlier-BUILD_TIME.sh  1660008162
 
+if [[ $# -eq 0 ]]; then
+	echo "Usage: # $(basename "$0")  1670008162  (must provide a timestamp)" && exit
+fi
+#shellcheck disable=2044 #(warning): For loops over find output are fragile. Use find -exec or a while read loop.
 for x in $(find /var/db/pkg -name BUILD_TIME); do
-	q=$(echo $x | sed 's|/var/db/pkg/||g');
-	q=$(echo $q | sed 's|/BUILD_TIME||g');
-	t=$(cat $x);
-	#compare numeric timestamp, if X earlier than $1, jle
-	if (( $t <= $1 )); then
-		echo $t $q;	#package is older.
+	btime=$(cat "$x");
+#shellcheck disable=2001 #(style): See if you can use ${variable//search/replace} instead.
+	cpv=$(echo "$x" | sed 's|/var/db/pkg/||g; s|/BUILD_TIME||g;');
+#	cpv=$(echo "$f" | sed 's|/BUILD_TIME||g');
+	#compare numeric timestamp, if X is earlier than first parameter $1, jle
+	if (( "$btime" <= "$1" )); then
+		echo "$btime" "$cpv";	#package is older.
 	fi;
 done
+
+#OUTPUT:
+#1689126989 acct-user/tss-0-r2
+#1675319868 app-admin/checksec-2.6.0-r1
+#1676041249 app-admin/doas-6.8.2
+
