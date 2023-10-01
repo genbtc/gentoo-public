26848a258dd23cd799d760ba0cddac5c96ae66d0
genBTC <genBTC@gmx.com>
2023-09-30 20:14:10 -0400

    shellcheck semakegit.sh

--- myscripts/semakegit.sh
+++ myscripts/semakegit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #@genr8eofl - gentoo (2023) July 24, Sept 30
-# semakegit.sh v0.2 - LICENSE CCBYSA 4.0
+# semakegit.sh v0.21 - LICENSE CCBYSA 4.0
 #orig oneliner: genr8too /etc/selinux/strict/policy # THISPROJECTNAME="gitupdate"; cp -u *.{te,if,fc} ../git/; cd ../git; git add . ; git commit -m `date +"%F_%H-%M-%S"`\ "${THISPROJECTNAME}" ; cd ../policy
 
 THISPROJECTNAME="$1" ;
-yes "yes" | cp -u -v *.{te,if,fc} ../git/ ;
-cd ../git ;
+yes "yes" | cp -u -v ./*.{te,if,fc} ../git/ ;
+cd ../git || exit;
 git add . ;
-git commit -m `date +"%F_%H-%M-%S"`\ "${THISPROJECTNAME}" ;
-cd ../policy ;
+git commit -m "$(date +'%F_%H-%M-%S')"\ "${THISPROJECTNAME}" ;
+cd ../policy || exit ;
