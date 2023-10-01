8902674d8d0fe0438c278ee2713875a326db4e02
genBTC <genBTC@gmx.com>
2023-09-30 20:40:34 -0400

    argument required for stage4-make-grub-rescue.sh

--- myscripts/stage4-make-grub-rescue.sh
+++ myscripts/stage4-make-grub-rescue.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-# make-grub-rescue-stage4.sh script 0.2 - created by genr8eofl @ gentoo - July 2-5, 2023 - GPL2
+# make-grub-rescue-stage4.sh script 0.3 - created by genr8eofl @ gentoo - July 2-5, 2023 - GPL2
+if [[ $# -eq 0 ]]; then
+    echo "Usage: # $(basename "$0") outputname.iso /path/outputdir/" && exit
+fi
 #example command:
 #grub-mkrescue -iso-level 3 -R -o /mnt/crucialp1/gentoostage4grubimage.iso /mnt/crucialp1/gentoo-minimal.iso_build/
 # HFS was causing file errors, disabled it through -- xorriso raw options
-grub-mkrescue -iso-level 3 -R -o ${1} ${2} -- -hfsplus off
+grub-mkrescue -iso-level 3 -R -o "${1}" "${2}" -- -hfsplus off
