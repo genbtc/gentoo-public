5baad3c6b5d1d077ba12e446c19c109d4a54c2b9
genBTC <genBTC@gmx.com>
2023-10-21 14:55:20 -0400

    AVC AUDIT

--- secureboot/1-make-gpg-armoredkeys-fifo.sh
+++ secureboot/1-make-gpg-armoredkeys-fifo.sh
@@ -1,4 +1,4 @@
 mkfifo /tmp/key_pipe & sleep 1 && for key_type in PK KEK db dbx; do
- openssl req -new -x509 -newkey rsa:4096 -subj "/CN=AVV Secureboot ${key_type}" -keyout /tmp/key_pipe -out ${key_type}.crt -days 9999 -nodes -sha512 &
+ openssl req -new -x509 -newkey rsa:4096 -subj "/CN=ABC Secureboot ${key_type}" -keyout /tmp/key_pipe -out ${key_type}.crt -days 9999 -nodes -sha512 &
   gpg --output ${key_type}.key.gpg --recipient secureboot@avv.com --encrypt < /tmp/key_pipe ;
 done ; rm /tmp/key_pipe
