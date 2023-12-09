9f16e41ab7a2885bcc9e931daa1a4033b1eb060f
genBTC <genBTC@gmx.com>
2023-12-09 12:56:16 -0500

    auditd rules, create IMA usage documentation text, kernel docs

--- kernel/netconsole.sh
+++ kernel/netconsole.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-modprobe netconsole netconsole=6666@192.168.1.119/enp5s0f1,6666@192.168.1.120/
+#TODO: get mac address
+modprobe netconsole netconsole=6666@192.168.1.119/enp5s0f1,6666@192.168.1.120/XX:XX:XX:XX:XX:XX
