9a056bc9105ed1dca550c60f24c98e062a4caa33
genBTC <genBTC@gmx.com>
2024-01-17 00:33:34 -0500

    redo script to support spaces/slashes by defining a clean file name as a 2nd paramter string

--- myscripts/sysgather.sh
+++ myscripts/sysgather.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
-#description: lsmod psaux dmesg cpuinfo gather -genr8eofl Feb2023
+#description: lsmod psaux dmesg cpuinfo gather v0.41 -genr8eofl Dec 2023
 datenow=$(date +"%s")
-pushd ~
 mkdir -p sysgather-logs
-cd sysgather-logs
+cd ~/sysgather-logs
 mkdir $datenow
 cd $datenow
 stringtofile() {
     $($1 > $1)
     head -n-0 $1
 }
-
-stringtofile "lspci"
+stringtofile2() {
+    $($1 > $2)
+    head -n-0 $2
+}
+stringtofile "uptime"
+stringtofile2 "uname -a" "uname-a"
 stringtofile "lsmod"
-stringtofile "ps faux --headers"
+stringtofile2 "ps faux --headers" "ps-faux"
 stringtofile "dmesg"
-stringtofile "cat /proc/cpuinfo"
+stringtofile2 "cat /proc/cpuinfo" "proc-cpuinfo"
 stringtofile "dmidecode"
-stringtofile "lspci -v"
+stringtofile "lspci"
+stringtofile2 "lspci -v" "lspci-v"
 stringtofile "lsusb"
-stringtofile "lsusb -v"
-stringtofile "cat /proc/interrupts"
+stringtofile2 "lsusb -v" "lsusb-v"
+stringtofile2 "cat /proc/interrupts" "proc-interrupts"
 stringtofile "df"
-
-popd
