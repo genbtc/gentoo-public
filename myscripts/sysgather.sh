ec7d16f1045b8cfbde97a2669ee4b4fc669e9302
genBTC <genBTC@gmx.com>
2024-01-17 01:23:46 -0500

    Add more commands/scripts to the gather list and fix typos from 2 to 1
    Signed-off-by: genBTC <genBTC@gmx.com>

--- myscripts/sysgather.sh
+++ myscripts/sysgather.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Description: lsmod psaux dmesg cpuinfo string to file gatherer tool v0.5 
+# Description: lsmod psaux dmesg cpuinfo string to file gatherer tool v0.51 
 # Author: genr8eofl @ gentoo - Dec 2023 + Jan 2024
 
 datenow=$(date +"%s")
@@ -16,13 +16,16 @@ stringtofile2() {
     head -n-0 $2
 }
 
+stringtofile "hostname"
 stringtofile "uptime"
 stringtofile2 "uname -a" "uname-a"
 stringtofile "dmesg"
 stringtofile "lsmod"
+stringtofile2 "cat /proc/cmdline" "proc-cmdline"
 
 stringtofile2 "cat /proc/interrupts" "proc-interrupts"
 stringtofile2 "cat /proc/cpuinfo" "proc-cpuinfo"
+stringtofile2 "grep 'Name\|Sysfs' /proc/bus/input/devices" "proc-bus-input-devices"
 stringtofile "lscpu"
 
 stringtofile "dmidecode"
@@ -32,6 +35,9 @@ stringtofile2 "lspci -v" "lspci-v"
 stringtofile "lsusb"
 stringtofile2 "lsusb -v" "lsusb-v"
 
+stringtofile "blkid"
+stringtofile "lsblk"
+stringtofile2 "lsblk -f" "lsblk-f"
 stringtofile "df"
 stringtofile "du"
 stringtofile "free"
@@ -39,12 +45,14 @@ stringtofile "mount"
 stringtofile "swapon"
 stringtofile2 "ps faux --headers" "ps-faux"
 
-stringtofile2 "emerge --info" "emerge-info"
-stringtofile2 "resolve-march-native"
-stringtofile2 "cpuid2cpuflags"
+stringtofile2 "cat /etc/os-release" "etc-os-release"
 
-stringtofile2 "nvme-cli smart-log /dev/root" "nvme-cli-smart-log-dev-root"
-stringtofile2 "smartctl -a /dev/root" "smartctl"
+stringtofile2 "emerge --info" "emerge-info"
+stringtofile "resolve-march-native"
+stringtofile "cpuid2cpuflags"
+
+stringtofile2 "nvme-cli smart-log /dev/root" "nvme-cli-smart-log-devroot"
+stringtofile2 "smartctl -a /dev/root" "smartctl-a-devroot"
 
 stringtofile2 "ip l" "ip-l"
 stringtofile2 "ip a" "ip-a"
