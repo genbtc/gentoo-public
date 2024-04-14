bdc2204131434a474807b8a892ecae613d97bc0a
genBTC <genBTC@gmx.com>
2024-04-14 10:06:24 -0400

    sysgather.sh: update script & check command for invalid commands

--- myscripts/sysgather.sh
+++ myscripts/sysgather.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
-# Description: lsmod psaux dmesg cpuinfo string to file gatherer tool v0.52
-# Author: genr8eofl @ gentoo - Dec 2023 + Jan 2024 - LICENSE AGPL3
+# Description: lsmod psaux dmesg cpuinfo string to file gatherer tool v0.53
+# Author: genr8eofl @ gentoo - Dec 2023 + Jan 2024 + Apr 2024 - LICENSE AGPL3
 
 datenow=$(date +"%s")
-mkdir -p sysgather-logs
+mkdir -pv ~/sysgather-logs
 cd ~/sysgather-logs
 mkdir $datenow
 cd $datenow
 stringtofile() {
+    if command -v $1 >/dev/null; then return 1; fi #command Not Found
     $($1 > $1)
     head -n-0 $1
 }
 stringtofile2() {
+    if command -v $1 >/dev/null; then return 1; fi #command Not Found
     $($1 > $2)
     head -n-0 $2
 }
@@ -25,6 +27,7 @@ stringtofile2 "cat /proc/cmdline" "proc-cmdline"
 
 stringtofile2 "cat /proc/interrupts" "proc-interrupts"
 stringtofile2 "cat /proc/cpuinfo" "proc-cpuinfo"
+stringtofile2 "cat /proc/iomem" "proc-iomem"
 stringtofile2 "grep 'Name\|Sysfs' /proc/bus/input/devices" "proc-bus-input-devices"
 stringtofile "lscpu"
 
@@ -47,15 +50,18 @@ stringtofile2 "ps faux --headers" "ps-faux"
 
 stringtofile2 "cat /etc/os-release" "etc-os-release"
 
+#IP
+stringtofile2 "ip l" "ip-l"
+stringtofile2 "ip a" "ip-a"
+stringtofile2 "ip r" "ip-r"
+
+#Gentoo
 stringtofile2 "emerge --info" "emerge-info"
 stringtofile "resolve-march-native"
 stringtofile "cpuid2cpuflags"
 
+#Disks (optional)
 stringtofile2 "nvme smart-log /dev/root" "nvme-smart-log-devroot"
 stringtofile2 "smartctl -a /dev/root" "smartctl-a-devroot"
-
-stringtofile2 "ip l" "ip-l"
-stringtofile2 "ip a" "ip-a"
-stringtofile2 "ip r" "ip-r"
 
 find . -size 0 -delete
